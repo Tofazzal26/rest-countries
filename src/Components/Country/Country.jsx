20c8f3d5a86e967aef0ada9e3a571d158c9cf323
Tofazzal26 <afranislamabir6789@gmail.com>
2024-03-11 09:27:29 +0600

    visited and going validation complete

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "../Country/Country.css";
 
 const Country = ({ countries }) => {
   console.log(countries);
-  const { name, flags } = countries;
+  const { name, flags, population, area, cca3 } = countries;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleClick = () => {
+    setVisited(!visited);
+  };
+
   return (
     <div className="box">
       <h2>Name: {name.common}</h2>
       <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      <p>Code: {cca3}</p>
+      <p>{visited ? "Visited this place" : "I am going this place"}</p>
+      <button onClick={handleClick}>{visited ? "Visited" : "Going"}</button>
     </div>
   );
 };
